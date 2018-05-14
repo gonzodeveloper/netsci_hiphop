c95770b9594608522ebba36d7678cd02db13997f
Wintergreen <doctor.kyle.hart@gmail.com>
2018-05-13 23:20:04 -1000

    more visuals

--- sctipts/load_graph_analyze.R
+++ sctipts/load_graph_analyze.R
@@ -67,27 +67,52 @@ write_graph(HH.artists, "graphs/techno_artists_fixed.graphml",
 #############################################################################
 
 # Number of verticies N
-(HH.N <- length(V(HH.artists)))        # 10117
-(TC.N <- length(V(TC.artists)))        # 6078
+(HH.N <- vcount(HH.artists))        # 10117
+(TC.N <- vcount(TC.artists))        # 6078
+
+# Number of Edges
+ecount(HH.artists)                  # 13845
+ecount(TC.artists)                  # 5688
+# Number of Components
+length(sort(components(HH.artists)$csize))  # 2499
+length(sort(components(TC.artists)$csize))  # 1859
+
+# Giant Componnet Hip Hop
+HH.components <- components(HH.artists)
+big <- which(HH.components$csize == max(HH.components$csize))
+HH.giant.nodes <- V(HH.artists)[which(HH.components$membership == big)]
+HH.giant <- induced_subgraph(HH.artists, HH.giant.nodes) 
+summary(HH.giant)          # |V| =  3856    |E| = 9301
+
+# Giant Component Techno
+TC.components <- components(TC.artists)
+big <- which(TC.components$csize == max(TC.components$csize))
+TC.giant.nodes <- V(TC.artists)[which(TC.components$membership == big)]
+TC.giant <- induced_subgraph(TC.artists, TC.giant.nodes) 
+summary(TC.giant)          # |V| = 1538     |E| = 2169
+
+
 # Average Degree <k>
 (HH.k <- mean(degree(HH.artists)))     # 2.736977
 (TC.k <- mean(degree(TC.artists)))     # 1.733465
-# Geodesic Distances
-mean_distance(HH.artists)              # 5.6226
-mean_distance(TC.artists)              # 9.3225
+
 
 # Calculating Regimes
 log(HH.N)             # With a random graph modeling these networks we would
 log(TC.N)             # expect both graphs to be in supercritical regime
 
-# Number of Components
-length(sort(components(HH.artists)$csize))  # 2499
-length(sort(components(TC.artists)$csize))  # 1859
 
-# Largest Component
-max(components(HH.artists)$csize)            # 3856
-max(components(TC.artists)$csize)            # 1538
+# Geodesic Distances
+mean_distance(HH.artists)              # 5.6226
+mean_distance(TC.artists)              # 9.3225
 
+# Small World?
+log(HH.N)/log(HH.k) 
+log(TC.N)/log(TC.k)
+
+# Diameter
+diameter(HH.artists)  # 26
+diameter(TC.artists)  # 33
 
 # Global Transitivity
 transitivity(HH.artists, type = "global")    # 0.23438  
@@ -111,8 +136,6 @@ V(HH.artists)$degree <- degree(HH.artists)
 V(HH.artists)$strength <-strength(HH.artists)
 V(HH.artists)$eigen_centrality <- eigen_centrality(HH.artists)$vector
 V(HH.artists)$page_rank <- page_rank(HH.artists)$vector
-V(HH.artists)$authority <- authority_score(HH.artists)$vector
-V(HH.artists)$hub       <- hub_score(HH.artists)$vector
 V(HH.artists)$betweenness <- betweenness(HH.artists, normalized=TRUE)
 V(HH.artists)$closeness <- closeness(HH.artists, normalized=TRUE)
 
@@ -120,8 +143,6 @@ V(TC.artists)$degree <- degree(TC.artists)
 V(TC.artists)$strength <-strength(TC.artists)
 V(TC.artists)$eigen_centrality <- eigen_centrality(TC.artists)$vector
 V(TC.artists)$page_rank <- page_rank(TC.artists)$vector
-V(TC.artists)$authority <- authority_score(TC.artists)$vector
-V(TC.artists)$hub       <- hub_score(TC.artists)$vector
 V(TC.artists)$betweenness <- betweenness(TC.artists, normalized=TRUE)
 V(TC.artists)$closeness <- closeness(TC.artists, normalized=TRUE)
 
@@ -146,16 +167,6 @@ topnv(HH.artists, V(HH.artists)$page_rank)$name
 #[4] "Lil' Jon"          "50 Cent"           "Pharrell Williams"
 #[7] "Busta Rhymes"      "Kanye West"        "Method Man"       
 #[10] "Nas"  
-topnv(HH.artists, V(HH.artists)$authority)$name
-#[1] "Ghostface Killah"  "Raekwon"           "Method Man"       
-#[4] "RZA"               "Inspectah Deck"    "GZA"              
-#[7] "Ol' Dirty Bastard" "Wu-Tang Clan"      "U-God"            
-#[10] "Masta Killa" 
-topnv(HH.artists, V(HH.artists)$hub)$name
-#[1] "Ghostface Killah"  "Raekwon"           "Method Man"       
-#[4] "RZA"               "Inspectah Deck"    "GZA"              
-#[7] "Ol' Dirty Bastard" "Wu-Tang Clan"      "U-God"            
-#[10] "Masta Killa" 
 topnv(HH.artists, V(HH.artists)$betweenness)$name
 #[1] "Snoop Dogg"        "Jay-Z"             "Nas"              
 #[4] "Busta Rhymes"      "KRS-One"           "Lil Wayne"        
@@ -192,16 +203,6 @@ topnv(TC.artists, V(TC.artists)$page_rank)$name
 #[5] "Ant"                  "Guy McAffer"         
 #[7] "Sterling Moss"        "Alex Calver"         
 #[9] "Gary Beck"            "Jack Wax" 
-topnv(TC.artists, V(TC.artists)$authority)$name
-#[1] "Adam Beyer"     "Cari Lekebusch" "Muslimgauze"   
-#[4] "Patrice Scott"  "Andy Vaz"       "Mentalic"      
-#[7] "Alex Hasten"    "powwowW"        "Micro Naps"    
-#[10] "Underspreche" 
-topnv(TC.artists, V(TC.artists)$hub)$name
-#[1] "Adam Beyer"     "Cari Lekebusch" "Muslimgauze"   
-#[4] "Patrice Scott"  "Andy Vaz"       "Mentalic"      
-#[7] "Alex Hasten"    "powwowW"        "Micro Naps"    
-#[10] "Underspreche"
 topnv(TC.artists, V(TC.artists)$betweenness)$name
 #[1] "Mark Broom"           "D.A.V.E. The Drummer"
 #[3] "Pounding Grooves"     "Ben Sims"            
@@ -219,22 +220,22 @@ topnv(TC.artists, V(TC.artists)$closeness)$name
 # Ego Networks for Hip Hop Only
 (snoop <- V(HH.artists)[which(V(HH.artists)$name=="Snoop Dogg")])
 (nas <- V(HH.artists)[which(V(HH.artists)$name=="Nas")])
-(jay_z <- V(HH.artists)[which(V(HH.artists)$name=="Jay-Z")])
+(lil_jon <- V(HH.artists)[which(V(HH.artists)$name=="Lil' Jon")])
 (ghostface <- V(HH.artists)[which(V(HH.artists)$name=="Ghostface Killah")])
 (pharell <- V(HH.artists)[which(V(HH.artists)$name=="Pharrell Williams")])
 
 
 HH.snoop <- make_and_plot_ego_graph(HH.artists, snoop, 3)
 HH.nas <- make_and_plot_ego_graph(HH.artists, nas, 3)
-HH.jay_z <- make_and_plot_ego_graph(HH.artists, jay_z, 3)
+HH.lil_jon <- make_and_plot_ego_graph(HH.artists, lil_jon, 3)
 HH.ghostface <- make_and_plot_ego_graph(HH.artists, ghostface, 3)
 HH.pharell <- make_and_plot_ego_graph(HH.artists, pharell, 3)
 
-write_graph(HH.snoop, "snoop.graphml", format = "graphml")
-write_graph(HH.nas, "nas.graphml", format = "graphml")
-write_graph(HH.jay_z, "jay-z.graphml", format = "graphml")
-write_graph(HH.ghostface, "ghostface.graphml", format = "graphml")
-write_graph(HH.pharell, "pharell.graphml", format = "graphml")
+write_graph(HH.snoop, "graphs/snoop.graphml", format = "graphml")
+write_graph(HH.nas, "graphs/nas.graphml", format = "graphml")
+write_graph(HH.lil_jon, "graphs/lil_jon.graphml", format = "graphml")
+write_graph(HH.ghostface, "graphs/ghostface.graphml", format = "graphml")
+write_graph(HH.pharell, "graphs/pharell.graphml", format = "graphml")
 
 
 
@@ -326,32 +327,25 @@ V(TC.artists)$Cluster_Infomap <- membership(TC.info)
 # ROBUSTNESS AND CASCADES
 #############################################################################
 
-HH.components <- components(HH.artists)
-big <- which(HH.components$csize == max(HH.components$csize))
-HH.giant.nodes <- V(HH.artists)[which(HH.components$membership == big)]
-HH.giant <- induced_subgraph(HH.artists, HH.giant.nodes) 
-summary(HH.giant)
-
-
-TC.components <- components(TC.artists)
-big <- which(TC.components$csize == max(TC.components$csize))
-TC.giant.nodes <- V(TC.artists)[which(TC.components$membership == big)]
-TC.giant <- induced_subgraph(TC.artists, TC.giant.nodes) 
-summary(TC.giant)
-
+# Robustness
+new_window(width=8,height=4)
+par(mfrow=c(1, 2))
 HH.failure.metrics <- robustness_simulation(HH.artists,  attack = TRUE, cutoff = .9)
 plot_robustness_results(HH.failure.metrics, "Attack on Hip Hop")
 
 HH.giant.failure.metrics <- robustness_simulation(HH.giant, attack = TRUE, cutoff = .9)
 plot_robustness_results(HH.giant.failure.metrics, "Attack on Hip Hop Main")
 
+new_window(width=8,height=4)
+par(mfrow=c(1, 2))
 TC.failure.metrics <- robustness_simulation(TC.artists,  attack = TRUE, cutoff = .9)
 plot_robustness_results(TC.failure.metrics, "Attack on Techno")
 
 TC.giant.failure.metrics <- robustness_simulation(TC.giant, attack = TRUE, cutoff = .9)
 plot_robustness_results(TC.giant.failure.metrics, "Attack on Techno Main")
 
-
+# SIR (SI)
+HH.sir <- sir(HH.artists, beta=0.2, gamma = 0, no.sim = 5)
 # Summarize and save hip hop
 summary(HH.artists)
 write_graph(HH.artists, "graphs/hip_hop_artists.graphml",
